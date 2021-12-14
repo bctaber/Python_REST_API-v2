51e67779c24ae3f8bcc44c240f523e3228a6ff22
Brent <bctaber@gmail.com>
2021-12-13 22:04:57 -0500

    Updated description at the top of the code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Second Python API tutorial
 #Ref: s://www.youtube.com/watch?v=GN6ICac3OXY&list=PLPZbGfThL-GWlUaanfM_QNkOtIjnoBNtq&index=4
-#Notes: Uses FASTAPI 
+#Notes: Uses FastAPI. Description: FastAPI is more recent compared to Flask and was released in 2018. It works similarly to Flask which supports the deployment of web applications with a minimal amount of code. However, FastAPI is faster compare to Flask as it is built on ASGI (Asynchronous Server Gateway Interface) whereby it supports concurrency / asynchronous code. This is done by declaring the endpoints with async def syntax.A good thing to highlight for FastAPI is the documentation. Upon deploying with FastAPI Framework, it will generate documentation and creates an interactive GUI (Swagger UI) which allows developers to test the API endpoints more conveniently.
+
+
 
