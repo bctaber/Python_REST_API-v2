ed76b8d94a25c1c374d9efc39c00cbccdaa5279f
bctaber <bctaber@gmail.com>
2021-12-14 23:14:12 -0500

    Reverted back from test

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 #Notes: Uses FastAPI. Description: FastAPI is more recent compared to Flask and was released in 2018. It works similarly to Flask which supports the deployment of web applications with a minimal amount of code. However, FastAPI is faster compare to Flask as it is built on ASGI (Asynchronous Server Gateway Interface) whereby it supports concurrency / asynchronous code. This is done by declaring the endpoints with async def syntax.A good thing to highlight for FastAPI is the documentation. Upon deploying with FastAPI Framework, it will generate documentation and creates an interactive GUI (Swagger UI) which allows developers to test the API endpoints more conveniently.
 
 
-y
+
